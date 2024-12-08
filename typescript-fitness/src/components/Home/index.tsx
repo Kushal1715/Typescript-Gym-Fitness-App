780a42ddcbb6e1b4802ab7a404957bcf45ee4658
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-08 22:03:12 +0545

    adding home page graphics

--- typescript-fitness/src/components/Home/index.tsx
+++ typescript-fitness/src/components/Home/index.tsx
@@ -75,6 +75,15 @@ const Home = ({ setSelectedPage }: Props) => {
           </motion.div>
         </div>
 
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
+
 
     </section>
   );
