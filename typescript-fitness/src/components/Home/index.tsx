34d47b86a387746f674652182ae0d8742904ae70
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-08 22:02:23 +0545

    adding action section in home page

--- typescript-fitness/src/components/Home/index.tsx
+++ typescript-fitness/src/components/Home/index.tsx
@@ -50,9 +50,34 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
 
+          {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
 
-        </section>
-        );
+
+    </section>
+  );
 };
 
-        export default Home;
+export default Home;
